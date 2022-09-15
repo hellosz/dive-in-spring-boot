0b8f779847381684d58c66df6b81325835da673f
田能 <neng.tian@patpat.com>
2022-09-15 20:03:07 +0800

    init project

---
+++ src/test/java/com/ralph/diveinspringboot/DiveInSpringBootApplicationTests.java
@@ -0,0 +1,10 @@
+package com.ralph.diveinspringboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveInSpringBootApplicationTests {
+
+
+}
